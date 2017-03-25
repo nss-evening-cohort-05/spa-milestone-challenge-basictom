b47e018db3fee3037454f5a03c485ca31fed0562
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-03-25 14:01:50 -0500

    stuck on clearing input field

--- js/activate-events.js
+++ js/activate-events.js
@@ -16,3 +16,25 @@ document.body.addEventListener("click", CarLot.addStyles);
 	return myCar;
 
 })(CarLot || {});
+
+
+//
+// var myInput = document.getElementById("txtField");
+//
+// if (myInput.value < 0 || myInput.value > 1000) {
+// myInput.value = '';
+// return outPut.innerHTML += "I said between 0 and 1000.";
+// }
+//
+// if (myInput.value == "") {
+// return outPut.innerHTML += "Please enter a number between 0 and 1000!!!";		 }
+//
+// if (myInput.value > randomNumber) {
+// myInput.value = '';
+// outPut.innerHTML += "Too High !!!";
+// }
+//
+// if (myInput.value < randomNumber) {
+// myInput.value = '';
+// outPut.innerHTML += "Too low !!!";
+// }
--- js/style-cars.js
+++ js/style-cars.js
@@ -1,6 +1,7 @@
 console.log("hello");
 var CarLot = (function(myCar){
   var newCar = document.getElementById("new-car");
+  var search = document.getElementById("search");
   var selectedDesc;
   console.log("not hitting if statement");
 
@@ -16,7 +17,7 @@ var CarLot = (function(myCar){
       selectedDesc = e.target.childNodes[0].childNodes[3];
     }else if(e.target.classList.contains("caption")){
       e.target.parentNode.classList.toggle("add-border");
-      console.log(e.target.childNodes[3]);
+      selectDesc = e.target.childNodes[3];
     }
     // }else if(e.target.classList.contains("thumbnail")){
     //   selectedDesc = e.target.childNodes[2].childNodes[1];
@@ -24,16 +25,22 @@ var CarLot = (function(myCar){
     // }else if(e.target.classList.contains("caption")){
     //   console.log(selectedDesc);
     // }
+    search.focus();
   }
 
   function removeBorder(e){
     var carArray = document.getElementsByClassName("thumbnail");
     for(var i=0;i<carArray.length;i++){
-      console.log(carArray[i]);
+      // console.log(carArray[i]);
       carArray[i].classList.remove("add-border");
+      search.innerHTML = ""
     }
   }
 
+  function mirrorText(){
+    selectDesc.innerHTML = search.value;
+  }
+search.addEventListener("keyup", mirrorText);
 document.body.addEventListener("click", CarLot.addStyles);
 
 })(CarLot || {});
